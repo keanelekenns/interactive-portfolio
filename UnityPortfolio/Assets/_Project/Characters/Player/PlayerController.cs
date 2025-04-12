9677fa79143b5081419f5b3c49bb95932bbc4f2d
Keanelek Enns <43152879+keanelekenns@users.noreply.github.com>
2025-04-11 23:10:21 -0600

    feat: make both movement systems work together

--- UnityPortfolio/Assets/_Project/Characters/Player/PlayerController.cs
+++ UnityPortfolio/Assets/_Project/Characters/Player/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f; // Movement speed
     private Vector2 moveInput; // Stores the movement input
+    private Vector3? destination = null; // Where the player wants to be
 
     private Rigidbody2D rb; // Rigidbody2D component for physics-based movement
     private PlayerInputActions playerActions;
@@ -28,26 +29,42 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    private void Update()
+    private void FixedUpdate()
     {
-        // Use the new input system to get movement input
-        moveInput = playerActions.Player.Move.ReadValue<Vector2>();
-
-        // Apply the movement based on input
         MoveCharacter();
     }
 
     private void MoveCharacter()
     {
-        // Move the character using the input and the rigidbody
-        rb.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        // Read any manual movement inputs
+        moveInput = playerActions.Player.Move.ReadValue<Vector2>();
+        if (moveInput != Vector2.zero) // Manual movement takes precedence
+        {
+            destination = null;
+            // Update the player's velocity with the manual controls
+            rb.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        }
+        else if (destination is Vector3 dest) // Move towards the destination
+        {
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, dest, moveSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(newPosition);
+            if (Vector3.Distance(transform.position, dest) < 0.05f)
+            {
+                // Once we arrive at our destination, we no longer have a destination
+                destination = null;
+            }
+        }
+        else // If no manual movements and no destination, stop moving
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 
     private void UpdatePlayerDestination(InputAction.CallbackContext context)
     {
-        Vector2 destination = context.ReadValue<Vector2>();
-        destination = Camera.main.ScreenToWorldPoint(destination);
-        Debug.Log("destination: " + destination.x + " " + destination.y);
-        transform.position = new Vector3(destination.x, destination.y, transform.position.z);
+        Vector2 dest = context.ReadValue<Vector2>();
+        Vector3 dest3d = Camera.main.ScreenToWorldPoint(dest);
+        Debug.Log("destination: " + dest3d.x + " " + dest3d.y + " " + dest3d.z);
+        destination = new Vector3(dest3d.x, dest3d.y, 0);
     }
 }
